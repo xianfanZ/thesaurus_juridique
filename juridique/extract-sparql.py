dd55c93a8ac60c5a2fdfc205fff3fea7eceee34b
xianfanZ <zhangxianfan.cheryl@gmail.com>
2017-04-11 11:05:09 +0200

    spaql

---
+++ juridique/extract-sparql.py
@@ -0,0 +1,45 @@
+from SPARQLWrapper import SPARQLWrapper,JSON
+
+cat1 = 'Branche du droit'
+
+
+def reqWrapper(cat):
+    sparql = SPARQLWrapper("http://fr.dbpedia.org/sparql")
+    req = 'select ?label where {?cat rdfs:label "' + cat + '"@fr .?subcat skos:broader ?cat.?subcat rdfs:label ?label.} LIMIT 1000'
+    sparql.setQuery(req)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    return results
+
+def reqRecursion(myResults):
+    #print(myResults)
+    if myResults["results"]["bindings"] != []:
+        for result in myResults["results"]["bindings"]:
+            print(result['label']['value'])
+            return reqRecursion(reqWrapper(result['label']['value']))
+    else:
+        return "--------cat fini------------"
+            #print(result["label"]["value"])
+
+
+
+sparql2 = SPARQLWrapper("http://fr.dbpedia.org/sparql")
+sparql2.setQuery("""
+    select ?label where {
+   ?cat rdfs:label "Système de castes en Inde"@fr .
+   ?subcat skos:broader ?cat.
+   ?subcat rdfs:label ?label.
+} LIMIT 1000
+""")
+print("#####################################")
+sparql2.setReturnFormat(JSON)
+results = sparql2.query().convert()
+print(results)
+if results["results"]["bindings"] == []:
+    print(True)
+
+#tree = parse(results)
+
+results1 = reqWrapper(cat1)
+#print(results1)
+print(reqRecursion(results1))
